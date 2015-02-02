fdbc8f67e591bd07224a57917101da9d5ac967a9
Allen <allenw1993@gmail.com>
2015-02-02 13:57:55 -0800

    dashboard
    dashboard added

---
+++ src/com/example/MRDD_Android/DashBoardActivity.java
@@ -0,0 +1,17 @@
+package com.example.MRDD_Android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Allen on 2/1/2015.
+ */
+public class DashBoardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+
+    }
+}
