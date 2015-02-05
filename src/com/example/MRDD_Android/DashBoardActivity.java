7ffd3f1ff95e2529d407affb26f131218546f3a6
Allen <allenw1993@gmail.com>
2015-02-04 21:55:47 -0800

    dashboard with listview
    dashboard now displays details of well in a list view

--- src/com/example/MRDD_Android/DashBoardActivity.java
+++ src/com/example/MRDD_Android/DashBoardActivity.java
@@ -1,17 +1,33 @@
 package com.example.MRDD_Android;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Allen on 2/1/2015.
  */
-public class DashBoardActivity extends Activity {
+public class DashBoardActivity extends Activity{
+
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
+        String[] details = new String[] {"Temp: 25 C", "Time : 0:25", "Depth: 500 ft"};
+        ArrayList<String> detailList = new ArrayList<String>();
+        detailList.addAll(Arrays.asList(details));
+
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, detailList);
+
+
+        setListAdapter(listAdapter);
 
     }
 }
